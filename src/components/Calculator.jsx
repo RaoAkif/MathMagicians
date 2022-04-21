6e43247794eeddeb38a5e50d003d394d53beb722
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-04-22 04:30:19 +0500

    Refactored with React Hooks

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 import buttonsArray from './buttonsArray';
 
-export default class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const Calculator = function () {
+  const [state, setState] = useState({});
 
-    this.state = {};
-  }
+  const handleButtonClick = useCallback((event) => {
+    setState((prev) => {
+      const output = calculate(prev, event.target.value);
+      return ({
+        ...prev,
+        ...output,
+      });
+    });
+  }, []);
 
-  handleButtonClick = (event) => {
-    this.setState((prevState) => calculate(prevState, event.target.value));
-  };
+  const { total, next, operation } = state;
+  const display = (total || '') + (operation || '') + (next || '');
 
-  formatOutput = () => {
-    const { total, next, operation } = this.state;
-    const display = (total || '') + (operation || '') + (next || '');
-
-    return display || '0';
-  };
-
-  render() {
-    return (
-      <div className="calculator">
-        <p className="calculator-screen">{this.formatOutput()}</p>
-        <div className="calculator-keys">
-          {Object.entries(buttonsArray).map(([i, { symbol, value, className }]) => (
-            <button onClick={this.handleButtonClick} key={i} type="button" className={className} value={value}>
-              {symbol}
-            </button>
-          ))}
-        </div>
+  return (
+    <div className="calculator">
+      <p className="calculator-screen">{display || '0'}</p>
+      <div className="calculator-keys">
+        {Object.entries(buttonsArray).map(([i, { symbol, value, className }]) => (
+          <button onClick={handleButtonClick} key={i} type="button" className={className} value={value}>
+            {symbol}
+          </button>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Calculator;
