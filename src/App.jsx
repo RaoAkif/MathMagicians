f9835eb7769f993702f12be725e4bd3f512b919e
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-04-29 01:01:04 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './routes/Home';
 import Quote from './routes/Quote';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quote" element={<Quote />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
 
 export default App;
